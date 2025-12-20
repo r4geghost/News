549b34de9e5e750bc7f14fde48e8bc1e795afdc1
Михаил Дюсов <143973@gmail.com>
2025-12-21 00:31:47 +0300

    update notification: show if notifications enabled in app settings

--- app/src/main/java/com/dyusov/news/data/background/RefreshDataWorker.kt
+++ app/src/main/java/com/dyusov/news/data/background/RefreshDataWorker.kt
@@ -6,22 +6,26 @@ import androidx.hilt.work.HiltWorker
 import androidx.work.CoroutineWorker
 import androidx.work.WorkerParameters
 import com.dyusov.news.domain.usecase.articles.UpdateSubscribedArticlesUseCase
+import com.dyusov.news.domain.usecase.settings.GetSettingsUseCase
 import dagger.assisted.Assisted
 import dagger.assisted.AssistedInject
+import kotlinx.coroutines.flow.first
 
 @HiltWorker
 class RefreshDataWorker @AssistedInject constructor(
     @Assisted context: Context,
     @Assisted workerParameters: WorkerParameters,
     private val updateSubscribedArticlesUseCase: UpdateSubscribedArticlesUseCase,
+    private val getSettingsUseCase: GetSettingsUseCase,
     private val notificationHelper: NotificationHelper
 ) : CoroutineWorker(context, workerParameters) {
 
     // this method will be running in worker
     override suspend fun doWork(): Result {
         Log.d("RefreshDataWorker", "Start")
+        val settings = getSettingsUseCase().first()
         val updatedTopics = updateSubscribedArticlesUseCase()
-        if (updatedTopics.isNotEmpty()) {
+        if (updatedTopics.isNotEmpty() && settings.notificationsEnabled) {
             notificationHelper.showNewArticlesNotification(updatedTopics)
         }
         Log.d("RefreshDataWorker", "Finish")
